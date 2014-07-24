f2932eb8cf0f6d5983a7b3360b6278830689178d
valentinsvt <valentinsvt@hotmail.com>
2014-07-24 01:29:23 -0500

    mapas funcionando

--- src/com/nth/ikiam/MapFragment.java
+++ src/com/nth/ikiam/MapFragment.java
@@ -5,19 +5,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by DELL on 23/07/2014.
  */
-public class MapFragment extends Fragment {
-
+public class MapFragment extends Fragment implements Button.OnClickListener {
+    private Button chooseBtn;
     public MapFragment() {
         // Empty constructor required for fragment subclasses
+        super();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.map_layout, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.map_layout, container, false);
+        chooseBtn = (Button) view.findViewById(R.id.btnLocate);
+        chooseBtn.setOnClickListener(this);
+        return view;
+
     }
+
+    @Override
+    public void onClick(View v){
+        System.out.println("click");
+    }
+
+
 }
