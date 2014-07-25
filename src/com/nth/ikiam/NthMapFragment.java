94de8310726834fca8acbbfe8ffbe2a41c2cdee8
valentinsvt <valentinsvt@hotmail.com>
2014-07-25 09:32:04 -0500

    algo del mapa creo

--- src/com/nth/ikiam/NthMapFragment.java
+++ src/com/nth/ikiam/NthMapFragment.java
@@ -1,6 +1,7 @@
 package com.nth.ikiam;
 
 import android.app.Fragment;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
  */
 public class NthMapFragment extends Fragment implements Button.OnClickListener {
     private Button chooseBtn;
+    private Button[] botones;
     private static GoogleMap map;
     private static LatLng location ;
     public NthMapFragment() {
@@ -25,8 +27,14 @@ public class NthMapFragment extends Fragment implements Button.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.map_layout, container, false);
-        chooseBtn = (Button) view.findViewById(R.id.btnLocate);
-        chooseBtn.setOnClickListener(this);
+        botones = new Button[2];
+        botones[0]=(Button) view.findViewById(R.id.btnGalapagos);
+        botones[1]= (Button) view.findViewById(R.id.btnLocate);
+       // chooseBtn = (Button) view.findViewById(R.id.btnGalapagos);
+        //chooseBtn.setOnClickListener(this);
+        for(int i=0;i<botones.length;i++){
+            botones[i].setOnClickListener(this);
+        }
         // map=((MapFragment) getFragmentManager().findFragmentById(R.id.mapF)).getMap();
         //  map=new MapFragment().getMap();
         setUpMapIfNeeded();
@@ -46,6 +54,22 @@ public class NthMapFragment extends Fragment implements Button.OnClickListener {
             if (map != null)
                 setUpMap();
         }
+    }
+    @Override
+    public void onClick(View v){
+        if(v.getId()==botones[0].getId()){
+            location=new LatLng( -0.4614207935306084, -90.615234375);
+            CameraUpdate update= CameraUpdateFactory.newLatLngZoom(location,7);
+            map.animateCamera(update);
+        }
+        if(v.getId()==botones[1].getId()){
+            Location myLocation  = map.getMyLocation();
+            location=new LatLng( myLocation.getLatitude(), myLocation.getLongitude());
+            CameraUpdate update= CameraUpdateFactory.newLatLngZoom(location,19);
+            map.animateCamera(update);
+
+        }
+
     }
 
     @Override
@@ -80,12 +104,9 @@ public class NthMapFragment extends Fragment implements Button.OnClickListener {
      * is not null.
      */
     private static void setUpMap() {
-
-        System.out.println("setUpMap");
         location=new LatLng(-1.6477220517969353, -78.46435546875);
         CameraUpdate update= CameraUpdateFactory.newLatLngZoom(location,6);
-
-        System.out.println("map "+map);
+        map.setMyLocationEnabled(true);
         map.animateCamera(update);
         // For showing a move to my loction button
         //map.setMyLocationEnabled(true);
@@ -95,15 +116,7 @@ public class NthMapFragment extends Fragment implements Button.OnClickListener {
         //map.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 12.0f));
     }
 
-    @Override
-    public void onClick(View v){
 
-        location=new LatLng( -0.4614207935306084, -90.615234375);
-        CameraUpdate update= CameraUpdateFactory.newLatLngZoom(location,7);
-
-
-        map.animateCamera(update);
-    }
 
 
 }
